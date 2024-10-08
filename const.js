7b40b9cf33324ab1ea86e95dac1cb671521c19c2
M.Suleman <153644774+Zeus123224@users.noreply.github.com>
2024-10-08 22:09:29 +0000

    datatypes

---
+++ const.js
@@ -0,0 +1,14 @@
+const accountid = 14453
+let accountEmail = "Hsuleman649@gmail.com"
+var accountPassword = "12345"
+accountcity="lahore"
+
+
+accountEmail = "hc@hc.com"
+accountPassword="2121212121"
+accountcity = "Bangladesh "
+console.log(accountid)
+
+console.table([accountid,accountEmail,accountPassword])
+/* prefer not to use var because of the issue of 
+in block scope and fuctional scope */
---
+++ datatype.js
@@ -0,0 +1,20 @@
+let name = "Suleman" //datatype is string
+let age = 21 // datatype = int
+let isLoggedIn = false //datatype = bool
+
+
+//number => 2 to power 53
+//Bigint 
+// string=> ""
+//boolean => true/false 
+//null= standaone value
+//undefined =>
+//symbol => unique
+
+
+//object
+
+
+console.log(typeof undefined    );//type undefined
+console.log(typeof null);// type is oject 
+
